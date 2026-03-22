232c2203aabf4a6a1b886f263859a61866b9c170
Leia Lai <leilai@c2r7s2.42lausanne.ch>
2026-03-22 17:14:56 +0100

    final gnl

--- 01/gnl/get_next_line.h
+++ 01/gnl/get_next_line.h
@@ -5,32 +5,25 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: leilai <marvin@42lausanne.ch>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/12/07 14:53:22 by leilai            #+#    #+#             */
-/*   Updated: 2025/12/07 15:08:42 by leilai           ###   ########.fr       */
+/*   Created: 2025/10/29 18:00:49 by leilai            #+#    #+#             */
+/*   Updated: 2025/11/29 17:29:46 by leilai           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
-
-# include <unistd.h>   // read, ssize_t
-# include <stdlib.h>   // malloc, free
-# include <limits.h>   // OPEN_MAX
-
 # ifndef BUFFER_SIZE
 #  define BUFFER_SIZE 42
 # endif
+# include <stdlib.h>
+# include <unistd.h>
+# include <fcntl.h>
 
-# ifndef MAX_FD
-#  define MAX_FD 1024
-# endif
-
-char    *get_next_line(int fd);
-
-size_t  ft_strlen(const char *s);
-char    *ft_strchr(const char *s, int c);
-char    *ft_strdup(const char *s);
-char    *ft_substr(char const *s, unsigned int st, size_t len);
-char    *ft_strjoin(char const *s1, char const *s2);
+char	*get_next_line(int fd);
+char	*ft_strdup(char *s);
+size_t	ft_strlen(char *s);
+char	*ft_substr(char *s, unsigned int st, size_t len);
+char	*ft_strjoin(char *s1, char *s2);
+void	fill_str(char *res, char *s1, char *s2);
 
 #endif
